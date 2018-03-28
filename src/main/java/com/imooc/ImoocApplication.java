b98083fe8848196beb293d78dbfdddeb230aef34
toryeLi <torey6061@qq.com>
2018-03-28 21:30:46 +0800

    SpringBoot项目初建
    SpringBoot测试

---
+++ src/main/java/com/imooc/ImoocApplication.java
@@ -0,0 +1,12 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//SpringBootApplication代表应用程序的住类
+@SpringBootApplication
+public class ImoocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImoocApplication.class, args);
+	}
+}
